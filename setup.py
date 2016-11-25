0e68fcf0b1ab9b6ddb8d59f8913ac3794a9b0c70
Vadim Markovtsev <vadim@sourced.tech>
2016-11-25 16:59:06 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name="jgscm",
     description="Jupyter Google Cloud Storage ContentsManager",
-    version="0.1.6",
+    version="0.1.7",
     license="MIT",
     author="Vadim Markovtsev",
     author_email="vadim@sourced.tech",
